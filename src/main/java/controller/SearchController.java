87d2353ae5be7fc923c2fb926d5d02094aaba3dc
backsofangels <backsofangels@gmail.com>
2017-12-04 19:00:18 +0100

    Create SearchController to handle all requests to database

---
+++ src/main/java/controller/SearchController.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Salvatore Penitente 2017.
+ */
+
+package main.java.controller;
+
+import main.java.model.DrillDAO;
+import main.java.model.MeasurementDAO;
+import main.java.model.PollutantDAO;
+import org.hibernate.SessionFactory;
+
+public class SearchController {
+    //funzinoe che mi fa la media degli inquinanti per una data sonda
+    //funzione che li formatta in un JSON
+
+    //endpoint delle ultime 5 misurazioni
+
+
+}
